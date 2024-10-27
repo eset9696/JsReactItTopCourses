3dcc869ceacaf5259e14770f690f558c446d5b15
Mikhail Sherkunkov <sherkunkov@gmail.com>
2024-10-27 23:06:33 +0800

    Обработано повторное добавление объекта в корзину

--- src/store/useProducts.js
+++ src/store/useProducts.js
@@ -6,7 +6,15 @@ const useProducts = create((set, get) => {
   const storedCart = JSON?.parse(localStorage?.getItem("cart")) || [];
 
   const addToCart = (product) => {
-    const updatedCart = [...get().cart, { ...product, quantity: 1 }];
+    let cart = [...get().cart];
+
+    const findedProduct = cart.find((element) => element.id === product.id);
+    if (findedProduct) {
+      findedProduct.quantity++;
+    } else {
+      cart = [...cart, { ...product, quantity: 1 }];
+    }
+    const updatedCart = cart;
 
     localStorage?.setItem("cart", JSON?.stringify(updatedCart));
 
@@ -14,7 +22,7 @@ const useProducts = create((set, get) => {
   };
 
   const removeFromCart = (id) => {
-    const updatedCart = get()?.cart?.filter((product) =>  product?.id !== id);
+    const updatedCart = get()?.cart?.filter((product) => product?.id !== id);
     localStorage?.setItem("cart", JSON?.stringify(updatedCart));
     set({ cart: updatedCart });
   };
